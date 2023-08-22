9f13db2c01969bc7a2976468de244b326d047170
NIG0181032 <quelmebiro@gmail.com>
2023-08-22 11:44:37 -0300

    cont

--- lista05.1/questao 08.py
+++ lista05.1/questao 08.py
@@ -1,3 +1,9 @@
 '''Desenvolver um programa que apresente todos os valores numéricos inteiros ímpares situados na faixa de 0 a
 20. Para saber se o número é ímpar, será necessário verificar essa condição com o comando if. Sendo ímpar,
 mostre-o; não sendo, passe para o próximo passo.'''
+
+num = 0
+
+while num <= 20: # enquanto num for menor ou igual a 20
+    print(num) # imprime o número ímpar
+num += 1 # incrementa o valor de num
